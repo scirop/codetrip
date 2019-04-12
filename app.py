f58ff06cfabdc914e2cdb6bb64949a4af9d0de68
Swarup Sahoo <swarupsahoo@Swarups-MacBook.local>
2019-04-11 19:22:04 -0500

    first push

---
+++ app.py
@@ -0,0 +1,13 @@
+# Code for running the codetrip website on heroku
+
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
