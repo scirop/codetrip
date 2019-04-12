2a5e6930cfd7c4388394cb616b4d73d328352df2
Swarup Sahoo <swarupsahoo@Swarups-MacBook.local>
2019-04-11 19:45:22 -0500

    changed routes

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def index():
+    return render_template('about.html')
+
+@app.route("/portfolio")
+def index():
+    return render_template('portfolio.html')
+
+@app.route("/contact")
+def index():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
